211b6eb652c744d0b2b208b4c2a611c67fac5b4b
Shawn Levin <shawnlevin22@gmail.com>
2018-02-01 14:14:34 -0800

    working on footer

--- src/components/home.js
+++ src/components/home.js
@@ -32,7 +32,7 @@ export default props => {
                 </div>
                 <div className="textcol col s9">
                     <h4>Welcome to MBoutique!</h4>
-                    <p className="middleText">We're a home-based baking business that specializes in the making of French macarons, a gluten free
+                    <p className="middleText">We're a home-based baking business that specializes in the making of French     macarons, a gluten free
                         pastry item made from ground almonds. Our business began at the West Reading Farmers Market in 2011.
                         Last year (2013) marked our third and final season of participation at the market.MBoutique was establishedto pay homage to the delicate French confectionery, the macaron. Our shop has been recognized as theconnoisseurs of this delicious French pastry because of the wonderful variety of flavors from our great master chefs.</p>
                     <h4>We love Macarons!</h4>
