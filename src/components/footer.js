592ce6b9b98a3bfd1bbe7984155e67b9715dd7bd
Shawn Levin <shawnlevin22@gmail.com>
2018-03-06 10:58:14 -0800

    fixed macarons picture

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,16 +10,16 @@ export default props => {
     return (
         <footer className="page-footer z-depth-2">
             <div className="row footerRow">
-                <div className="col l3 s12 footerLeft">
+                <div className="col l3 s12 footerLeft valign-wrapper">
                     <img src={mail} className="mailImg" />
                     <p>order@mboutique.com</p>
                 </div>
-                <div className="col l6 s12 footerMiddle">
+                <div className="col l6 s12 footerMiddle valign-wrapper">
                     <img src={phone} className="phoneImg" />
-                    <p className="phone">949.900.3111</p>
-                    <p className="copyR">COPYRIGHT @ 2017 MBOUTIQUE. ALL RIGHTS RESERVED</p>
+                    <h6 className="phone">949.900.3111</h6>
+                    <h6 className="copyR">COPYRIGHT @ 2017 MBOUTIQUE. ALL RIGHTS RESERVED</h6>
                 </div>
-                <div className="col l3 s12 footerRight">
+                <div className="col l3 s12 footerRight valign-wrapper">
                     <img src={twitter} className="twitterImg" />
                     <img src={facebook} className="facebookImg" />
                     <p>Follow us</p>
