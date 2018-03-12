fbf812a00ae9d8676d489cc088dd1ad451ad9225
Shawn Levin <shawnlevin22@gmail.com>
2018-03-11 22:41:07 -0700

    corrected input placeholder on contact from Message

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -49,10 +49,10 @@ class ContactForm extends Component {
         const { firstName, message, phone, email } = this.state.form;
         return (
             <form className="col l7 s12" onSubmit={this.handleSubmit} >
-                <Field name="firstName" label="First Name" type="text" value={firstName} onChange={this.handleInputChange} />
+                <Field name="firstName" label="Name" type="text" value={firstName} onChange={this.handleInputChange} />
                 <Field name="phone" label="Phone Number" type="tel" value={phone} onChange={this.handleInputChange} />
                 <Field name="email" label="Email" type="email" value={email} onChange={this.handleInputChange} />
-                <Field name="message" label="message" type="text" value={message} onChange={this.handleInputChange} />
+                <Field name="message" label="Message" type="text" value={message} onChange={this.handleInputChange} />
                 <button className="waves-effect waves-light btn-small light-blue darken-1">Submit</button>
                 <button type="button" className="waves-effect waves-light btn-small red accent-4 clearBtn" onClick={this.reset}>Clear Info</button>
             </form>
