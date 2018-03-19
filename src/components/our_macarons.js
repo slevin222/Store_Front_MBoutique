a05c0c7e1c06bdedb281f88be2dc83fe437658f2
Shawn Levin <shawnlevin22@gmail.com>
2018-03-18 22:12:54 -0700

    added logo and macarons pic to slide out mobile nav

--- src/components/our_macarons.js
+++ src/components/our_macarons.js
@@ -11,54 +11,54 @@ export default props => {
                     <img src={ourMacarons} className="ourMacaronsImg z-depth-1 " />
                 </div>
             </div>
-            <div className="row">
-                <div className="col s12" >
-                    <h4>Our Macarons are freshly made by hand</h4>
-                    <p>The vivid hues or our unique macarons reflect the natural flavors and essences that infuse the ganache filling of these delicious almond cookies - each has its own personality, and all of them are made to savour for their delicasy and unique character</p>
-                </div>
+            <div className="row"></div>
+            <div className="col s12" >
+                <h4>Our Macarons are freshly made by hand</h4>
+                <p>The vivid hues or our unique macarons reflect the natural flavors and essences that infuse the ganache filling of these delicious almond cookies - each has its own personality, and all of them are made to savour for their delicasy and unique character</p>
             </div>
+
             <div className="row">
                 <div className='col s6 l3 offset-s3 hide-on-med-and-down'>
                     <h4>Classics</h4>
                     <ul className="browser-default">
-                        <li>Caribbean Chocolate</li>
-                        <li>Coconut</li>
-                        <li>Colombian Coffee</li>
-                        <li>Green Tea</li>
-                        <li>Lemon</li>
-                        <li>Madagascar Vanilla</li>
-                        <li>Passion Fruit</li>
-                        <li>Rose</li>
-                        <li>Raspberry</li>
-                        <li>Tiffany Blue</li>
-                        <li>Sicilian Pistachio</li>
-                        <li>Sweet Wedding Almond</li>
-                        <li>Violet Cassis</li>
-                        <li>Salted Caramel</li>
+                        <li>caribbean chocolate</li>
+                        <li>coconut</li>
+                        <li>colombian coffee</li>
+                        <li>green tea</li>
+                        <li>lemon</li>
+                        <li>madagascar vanilla</li>
+                        <li>passion fruit</li>
+                        <li>rose</li>
+                        <li>raspberry</li>
+                        <li>tiffany blue</li>
+                        <li>sicilian pistachio</li>
+                        <li>sweet wedding almond</li>
+                        <li>violet cassis</li>
+                        <li>salted caramel</li>
                     </ul>
                 </div>
                 <div className="col s12 hide-on-med-and-up show-on-medium-and-down">
                     <h4 className="center-align">Classics</h4>
                     <div className="col s6">
                         <ul className="browser-default">
-                            <li>Caribbean Chocolate</li>
-                            <li>Coconut</li>
-                            <li>Colombian Coffee</li>
-                            <li>Green Tea</li>
-                            <li>Lemon</li>
-                            <li>Madagascar Vanilla</li>
-                            <li>Passion Fruit</li>
+                            <li>caribbean chocolate</li>
+                            <li>coconut</li>
+                            <li>colombian coffee</li>
+                            <li>green tea</li>
+                            <li>lemon</li>
+                            <li>madagascar vanilla</li>
+                            <li>passion fruit</li>
                         </ul>
                     </div>
                     <div className="col s6">
                         <ul className="browser-default">
-                            <li>Rose</li>
-                            <li>Raspberry</li>
-                            <li>Tiffany Blue</li>
-                            <li>Sicilian Pistachio</li>
-                            <li>Sweet Wedding Almond</li>
-                            <li>Violet Cassis</li>
-                            <li>Salted Caramel</li>
+                            <li>rose</li>
+                            <li>raspberry</li>
+                            <li>tiffany blue</li>
+                            <li>sicilian pistachio</li>
+                            <li>sweet wedding almond</li>
+                            <li>violet cassis</li>
+                            <li>salted caramel</li>
                         </ul>
                     </div>
                 </div>
