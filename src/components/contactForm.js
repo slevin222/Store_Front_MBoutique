b9457077a72d5cd32ebec52bae1f6041d3f6c6ea
Shawn Levin <shawnlevin22@gmail.com>
2018-03-25 22:14:07 -0700

    changing valication code

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import Field from './contactFields.js';
-import FormErrors from './FormErrors.js';
-
 
 
 class ContactForm extends Component {
@@ -13,15 +11,9 @@ class ContactForm extends Component {
                 phone: ' ',
                 email: ' ',
                 message: ' ',
-                formErrors: { Name: '', Phone: '', Email: '', Message: '' },
-                nameValid: false,
-                phoneValid: false,
-                emailValid: false,
-                messageValid: false,
                 submit: false
             }
         };
-
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
         this.reset = this.reset.bind(this);
@@ -34,14 +26,7 @@ class ContactForm extends Component {
                 phone: '',
                 email: '',
                 message: '',
-                formErrors: { Name: '', Phone: '', Email: '', Message: '' },
-                nameValid: false,
-                phoneValid: false,
-                emailValid: false,
-                messageValid: false,
                 submit: false,
-                formValid: false
-
             }
         });
     }
@@ -52,62 +37,14 @@ class ContactForm extends Component {
         form[name] = value;
         this.setState({
             form: { ...form }
-        }, () => {
-            this.validateField(name, value)
-        });
 
-    }
-
-    validateField(fieldName, value) {
-        let fieldValidationErrors = this.state.form.formErrors;
-        let emailValid = this.state.form.emailValid;
-        let nameValid = this.state.form.nameValid;
-        let phoneValid = this.state.form.phoneValid;
-        let messageValid = this.state.form.messageValid;
-
-        switch (fieldName) {
-            case 'email':
-                emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-                fieldValidationErrors.email = emailValid ? '' : ' is invalid. ';
-                break;
-            case 'name':
-                nameValid = value.length > 1;
-                fieldValidationErrors.name = nameValid ? '' : ' must be entered. ';
-                break;
-            case 'phone':
-                var phoneRegEx = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
-                phoneValid = value.match(phoneRegEx);
-                fieldValidationErrors.phone = phoneValid ? '' : ' is invalid. ';
-                break;
-            case 'message':
-                messageValid = value.length >= 15;
-                fieldValidationErrors.message = messageValid ? '' : ' is too short. ';
-                break;
-            default:
-                break;
-        }
-        this.setState({
-            form: {
-                formErrors: fieldValidationErrors,
-                emailValid: emailValid,
-                nameValid: nameValid,
-                phonelValid: phoneValid,
-                messageValid: messageValid,
-            }
-        }, this.validateForm);
-    }
-
-    validateForm() {
-        this.setState({
-            form: {
-                formValid: this.state.form.emailValid && this.state.form.nameValid && this.state.form.nameValid && this.state.form.messageValid,
-                submit: true
-            }
         });
     }
+
 
     handleSubmit(event) {
         event.preventDefault();
+
         console.log('handleSubmit called, form values are:', this.state.form);
         this.setState({
             form: {
@@ -115,24 +52,15 @@ class ContactForm extends Component {
                 phone: '',
                 email: '',
                 message: '',
-                formErrors: { name: '', phone: '', email: '', message: '' },
-                nameValid: false,
-                phoneValid: false,
-                emailValid: false,
-                messageValid: false,
                 submit: true
-
             }
         });
-
     }
 
     render() {
-        const { name, message, phone, email, formErrors, displayModal, submit, formValid } = this.state.form;
-
+        const { name, message, phone, email, submit, } = this.state.form;
         return (
             <form className="col l6 s12" onSubmit={this.handleSubmit} >
-                <FormErrors formErrors={formErrors} />
                 <Field id="name" name="name" label="Name" type="text" value={name} onChange={this.handleInputChange} />
                 <Field id="phone" name="phone" label="Phone Number" type="tel" value={phone} onChange={this.handleInputChange} />
                 <Field id="email" name="email" label="Email" type="text" value={email} onChange={this.handleInputChange} />
